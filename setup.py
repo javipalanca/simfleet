6610dc924053731fd7b107f760dac0e78ba32033
jpalanca <jpalanca@dsic.upv.es>
2017-11-15 00:12:55 +0100

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='taxi_simulator',
-    version='0.1.2',
+    version='0.1.3',
     description="Agent-based taxi simulator to test strategies",
     long_description=readme + '\n\n' + history,
     author="Javi Palanca",

e5a31f32cf3aab496160842204799821db83c255
cvcalderon <ccalderon@upv.es>
2024-07-01 13:30:24 +0200

    QueueStation v0.3 - Create CheckNearBehaviour and adapt code

--- simfleet/common/queuestationagent.py
+++ simfleet/common/queuestationagent.py
@@ -5,7 +5,7 @@ from loguru import logger
 from collections import deque
 from spade.message import Message
 from spade.template import Template
-from spade.behaviour import CyclicBehaviour
+from spade.behaviour import CyclicBehaviour, OneShotBehaviour
 
 from simfleet.common.geolocatedagent import GeoLocatedAgent
 
@@ -30,7 +30,8 @@ class QueueStationAgent(GeoLocatedAgent):
         self.services_list = {}
         self.waiting_lists = {}
 
-        self.near_list = {}
+        # ORIGINAL NEAR
+        #self.near_list = {}
 
         #statics
         self.transports_in_queue_time = 0
@@ -38,6 +39,25 @@ class QueueStationAgent(GeoLocatedAgent):
         self.total_busy_time = 0  # total time with some transport waiting in queue
         self.queue_length = 0
         self.max_queue_length = 0
+
+
+    async def setup(self):
+        logger.info("Queue agent {} running".format(self.name))
+
+        template1 = Template()
+        template1.set_metadata("protocol", REQUEST_PROTOCOL)
+        template1.set_metadata("performative", REQUEST_PERFORMATIVE) #template slo permite una sola performativa y protocolo
+
+        template2 = Template()
+        template2.set_metadata("protocol", REQUEST_PROTOCOL)
+        template2.set_metadata("performative", CANCEL_PERFORMATIVE)
+
+        #template3 = Template()
+        #template3.set_metadata("protocol", COORDINATION_PROTOCOL)
+        #template3.set_metadata("performative", INFORM_PERFORMATIVE)
+
+        #self.add_behaviour(self.queuebehaviour, template1 | template2 | template3)
+        self.add_behaviour(self.queuebehaviour, template1 | template2)
 
     # Service Management
     def add_service(self, service_name, slots, one_shot_behaviour, **arguments):
@@ -95,7 +115,6 @@ class QueueStationAgent(GeoLocatedAgent):
 
     def show_service_arguments(self, service_name):
         return self.services_list[service_name]["args"]
-
 
     async def send_inform_service(self, agent_id, content):
         """
@@ -293,72 +312,91 @@ class QueueStationAgent(GeoLocatedAgent):
                     if "args" in content:
                         arguments = content["args"]
 
-                    if str(agent_id) not in self.agent.near_list:
-                        self.agent.near_list[str(agent_id)] = {
-                            'service_name': service_name,
-                            'arguments': arguments
-                        }
-
-                        logger.warning(
-                            "DICCIONARIO NEAR: {} exists in the near_list - agent_id: {}.".format(
-                                self.agent.near_list,
-                                agent_id
-                            )
-                        )
-
-                    else:
-                        logger.warning(
-                            "The agent {} exists in the near_list.".format(
-                                agent_id
-                            )
-                        )
+                    # ORIGINAL NEAR
+                    # if str(agent_id) not in self.agent.near_list:
+                    #     self.agent.near_list[str(agent_id)] = {
+                    #         'service_name': service_name,
+                    #         'arguments': arguments
+                    #     }
+                    #
+                    #     logger.warning(
+                    #         "DICCIONARIO NEAR: {} exists in the near_list - agent_id: {}.".format(
+                    #             self.agent.near_list,
+                    #             agent_id
+                    #         )
+                    #     )
+                    #
+                    # else:
+                    #     logger.warning(
+                    #         "The agent {} exists in the near_list.".format(
+                    #             agent_id
+                    #         )
+                    #     )
 
                     #Comprobamos la ubicación
                     #content = {"user_agent_id": agent_id}
 
+                    #ORIGINAL NEAR
                     #Bus Line
-                    content = {"user_agent_id": agent_id, "object_type": object_type}
-                    await self.agent.request_agent_position("simulator_none@localhost", content)    #Cambiarlo - JID simulador - DEBATIR
+                    #content = {"user_agent_id": agent_id, "object_type": object_type}
+                    #await self.agent.request_agent_position("simulator_none@localhost", content)    #Cambiarlo - JID simulador - DEBATIR
 
                     # Request to SimulatorAgent for agent_position
                     #msg = await self.receive(timeout=5)         #Duda PREGUNTAR
 
-                elif (
-                        protocol == COORDINATION_PROTOCOL and performative == INFORM_PERFORMATIVE
-                ):  # comes from send_confirmation_travel
+                    #NEW NEAR
+                    template3 = Template()
+                    template3.set_metadata("protocol", COORDINATION_PROTOCOL)
+                    template3.set_metadata("performative", INFORM_PERFORMATIVE)
 
-                    if "agent_position" in content:
-                        agent_position = content["agent_position"]
+                    instance = CheckNearBehaviour(agent_id, service_name, object_type, arguments)
+                    self.agent.add_behaviour(instance, template3)
 
-                    if "user_agent_id" in content:
-                        user_agent_id = content["user_agent_id"]
+                    await instance.join()       # Wait for the behaviour to complete
 
-                    logger.warning(
-                        "NEAR depuracion - agent_position: {}, user_agent_id {}".format(
-                            agent_position,
-                            user_agent_id
-                        )
-                    )
+                    service_name = instance.service_name
+                    agent_position = instance.agent_position
+                    user_agent_id = instance.user_agent_id
+                    arguments = instance.arguments
 
-                    logger.warning(
-                        "NEAR_LIST: {} ".format(
-                            self.agent.near_list[user_agent_id]
-                        )
-                    )
+                # ORIGINAL - NEAR
+                # elif (
+                #         protocol == COORDINATION_PROTOCOL and performative == INFORM_PERFORMATIVE
+                # ):  # comes from send_confirmation_travel
+                #
+                #     if "agent_position" in content:
+                #         agent_position = content["agent_position"]
+                #
+                #     if "user_agent_id" in content:
+                #         user_agent_id = content["user_agent_id"]
+                #
+                #     logger.warning(
+                #         "NEAR depuracion - agent_position: {}, user_agent_id {}".format(
+                #             agent_position,
+                #             user_agent_id
+                #         )
+                #     )
+                #
+                #     logger.warning(
+                #         "NEAR_LIST: {} ".format(
+                #             self.agent.near_list[user_agent_id]
+                #         )
+                #     )
+                #
+                #     service_name = self.agent.near_list[user_agent_id]["service_name"]
+                #     arguments = self.agent.near_list[user_agent_id]["arguments"]
+                #     del self.agent.near_list[user_agent_id]
+                #
+                #     logger.debug(
+                #         "Station {} has received msg from agent {} for near check".format(
+                #             self.agent.name,
+                #             agent_id
+                #         )
+                #     )
 
-                    service_name = self.agent.near_list[user_agent_id]["service_name"]
-                    arguments = self.agent.near_list[user_agent_id]["arguments"]
-                    del self.agent.near_list[user_agent_id]
 
-                    logger.debug(
-                        "Station {} has received msg from agent {} for near check".format(
-                            self.agent.name,
-                            agent_id
-                        )
-                    )
-
-
-                    if service_name not in self.agent.waiting_lists or not self.agent.near_agent(coords_1=self.agent.get_position(), coords_2=agent_position):    #New
+                    if service_name not in self.agent.waiting_lists or not self.agent.near_agent(
+                            coords_1=self.agent.get_position(), coords_2=agent_position):    #New
 
                         await self.refuse_request_agent(user_agent_id)
                         logger.warning(
@@ -499,21 +537,119 @@ class QueueStationAgent(GeoLocatedAgent):
             #             self.agent.empty_queue_time - self.agent.transports_in_queue_time
             #         )
 
+class CheckNearBehaviour(OneShotBehaviour):
+    def __init__(self, user_agent_id, service_name, object_type, arguments):
+        super().__init__()
 
+        self.agent_simulator_id = "simulator_none@localhost"
+        self.user_agent_id = user_agent_id
+        self.service_name = service_name
+        self.object_type = object_type
+        self.arguments = arguments
+        self.agent_position = None
 
-    async def setup(self):
-        logger.info("Queue agent {} running".format(self.name))
+        #if 'transport_need' in kwargs:
+        #    self.transport_need = kwargs['transport_need']
 
-        template1 = Template()
-        template1.set_metadata("protocol", REQUEST_PROTOCOL)
-        template1.set_metadata("performative", REQUEST_PERFORMATIVE) #template slo permite una sola performativa y protocolo
+    async def request_agent_position_near(self, agent_id, content):
+        reply = Message()
+        reply.to = str(agent_id)
+        reply.set_metadata("protocol", COORDINATION_PROTOCOL)
+        reply.set_metadata("performative", REQUEST_PERFORMATIVE)
+        reply.body = json.dumps(content)
+        await self.send(reply)
 
-        template2 = Template()
-        template2.set_metadata("protocol", REQUEST_PROTOCOL)
-        template2.set_metadata("performative", CANCEL_PERFORMATIVE)
+    async def run(self):
 
-        template3 = Template()
-        template3.set_metadata("protocol", COORDINATION_PROTOCOL)
-        template3.set_metadata("performative", INFORM_PERFORMATIVE)
+        content = {"user_agent_id": self.user_agent_id, "object_type": self.object_type}
+        await self.request_agent_position_near(agent_id=self.agent_simulator_id, content=content)
 
-        self.add_behaviour(self.queuebehaviour, template1 | template2 | template3)
+        msg = await self.receive(timeout=30)
+
+        if msg:
+            performative = msg.get_metadata("performative")
+            protocol = msg.get_metadata("protocol")
+            agent_id = msg.sender
+            content = json.loads(msg.body)
+
+            if (
+                    protocol == COORDINATION_PROTOCOL and performative == INFORM_PERFORMATIVE
+            ):  # comes from send_confirmation_travel
+
+                if "agent_position" in content:
+                    agent_position = content["agent_position"]
+
+                if "user_agent_id" in content:
+                    user_agent_id = content["user_agent_id"]
+
+                logger.warning(
+                    "NEAR depuracion - agent_position: {}, user_agent_id {}".format(
+                        agent_position,
+                        user_agent_id
+                    )
+                )
+
+                #logger.warning(
+                #    "NEAR_LIST: {} ".format(
+                #        self.agent.near_list[user_agent_id]
+                #    )
+                #)
+
+                # Dentro del objeto
+                #service_name = self.agent.near_list[user_agent_id]["service_name"]
+                #arguments = self.agent.near_list[user_agent_id]["arguments"]
+                #del self.agent.near_list[user_agent_id]
+
+                logger.debug(
+                    "Station {} has received msg from agent {} for near check".format(
+                        self.agent.name,
+                        agent_id
+                    )
+                )
+
+                self.agent_position = agent_position
+                #self.result = (self.user_agent_id, self.service_name, agent_position, self.arguments)
+                #return (self.user_agent_id, self.service_name, agent_position, self.arguments)
+
+                # Fuera del objeto
+                # if service_name not in self.agent.waiting_lists or not self.agent.near_agent(
+                #         coords_1=self.agent.get_position(), coords_2=agent_position):  # New
+                #
+                #     await self.refuse_request_agent(user_agent_id)
+                #     logger.warning(
+                #         "Station {} has REFUSED request from agent {} for service {}".format(
+                #             self.agent.name,
+                #             user_agent_id,
+                #             service_name
+                #         )
+                #     )
+                # else:
+                #
+                #     if self.total_queue_size(service_name) == 0:
+                #         self.agent.transports_in_queue_time = time.time()
+                #
+                #     # Encolamos
+                #     self.queue_agent_to_waiting_list(service_name, str(user_agent_id), **arguments)
+                #     # self.queue_agent_to_waiting_list(service_name, str(agent_id), *arguments)     #Duda ARGS
+                #
+                #     if self.total_queue_size(service_name) > self.agent.max_queue_length:
+                #         self.agent.max_queue_length = self.total_queue_size(service_name)
+                #
+                #     content = {"station_id": str(self.agent.jid)}
+                #     await self.accept_request_agent(user_agent_id, content)
+                #
+                #     logger.info(
+                #         "Station {} has put {} in the waiting_list".format(
+                #             self.agent.name,
+                #             user_agent_id,
+                #         )
+                #     )
+            else:
+                logger.warning(
+                    "Station {} has not received agent position of {} from the Simulator".format(
+                        self.agent.name,
+                        agent_id,
+                    )
+                )
+
+        #return

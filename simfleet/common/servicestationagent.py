db1b07fe19b74fefca7a79bcf371db0b0c4bcb3b
cvcalderon <ccalderon@upv.es>
2024-10-18 10:28:57 +0200

    Code cleaning part1 - servicestationagent

--- simfleet/common/servicestationagent.py
+++ simfleet/common/servicestationagent.py
@@ -1,9 +1,7 @@
 import json
-import datetime
-import asyncio
 
 from loguru import logger
-from spade.behaviour import CyclicBehaviour, OneShotBehaviour, TimeoutBehaviour
+from spade.behaviour import CyclicBehaviour
 from spade.message import Message
 from spade.template import Template
 
@@ -11,60 +9,72 @@ from simfleet.common.queuestationagent import QueueStationAgent
 
 from simfleet.communications.protocol import (
     REQUEST_PROTOCOL,
-    ACCEPT_PERFORMATIVE,
-    CANCEL_PERFORMATIVE,
     REFUSE_PERFORMATIVE,
     INFORM_PERFORMATIVE,
-    COORDINATION_PROTOCOL,
 )
 
 class ServiceStationAgent(QueueStationAgent):
+    """
+        ServiceStationAgent is responsible for managing service stations (e.g., charging or refueling stations).
+        It extends the QueueStationAgent class, which allows agents to queue for available services.
+
+        Methods:
+            setup(): Initializes the service station agent and starts its behavior.
+            increase_slots_used(service_type): Increases the number of slots currently in use for a specific service type.
+            decrease_slots_used(service_type): Decreases the number of slots currently in use for a specific service type.
+            get_slot_number(service_type): Returns the total number of slots available for a specific service type.
+            get_slot_number_used(service_type): Returns the number of slots currently in use for a specific service type.
+            service_available(service_name): Checks if there are free slots available for a specific service.
+    """
+
     def __init__(self, agentjid, password):
         QueueStationAgent.__init__(self, agentjid, password)
 
 
     async def setup(self):
+        """
+            Initializes the service station and adds its main behavior.
+        """
         await super().setup()
         logger.info("Service station agent {} running".format(self.name))
-
-        #template1 = Template()
-        #template1.set_metadata("protocol", REQUEST_PROTOCOL)
-        #template1.set_metadata("performative", ACCEPT_PERFORMATIVE)
-
-        #template2 = Template()
-        #template2.set_metadata("protocol", REQUEST_PROTOCOL)
-        #template2.set_metadata("performative", CANCEL_PERFORMATIVE)
-
-        #self.add_behaviour(self.ServiceRunBehaviour(), template1 | template2)
-
         self.add_behaviour(self.ServiceRunBehaviour())
-        #Template for CANCEL y ACCEPT
 
-    #Ajustar nombers de los def
-    #def increase_service_slot(self, service_type):
     def increase_slots_used(self, service_type):
+        """
+            Increments the number of slots currently in use for a given service type.
+        """
         if service_type in self.services_list:
             self.services_list[service_type]["slots_in_use"] += 1
 
-    #def decrease_service_slot(self, service_type):
     def decrease_slots_used(self, service_type):
+        """
+            Decrements the number of slots currently in use for a given service type.
+        """
         if service_type in self.services_list:
             self.services_list[service_type]["slots_in_use"] -= 1
 
-    #def get_service_slot_number(self, service_type):
     def get_slot_number(self, service_type):
+        """
+            Returns the total number of slots for the given service type.
+        """
         return self.services_list[service_type]["slots"]
 
     def get_slot_number_used(self, service_type):
+        """
+            Returns the number of slots currently in use for the given service type.
+        """
         return self.services_list[service_type]["slots_in_use"]
 
     def service_available(self, service_name):
+        """
+            Checks if there are available slots for the given service name.
+            Returns:
+                bool: True if the service has available slots, False otherwise.
+        """
         if self.services_list[service_name]["slots_in_use"] >= self.services_list[service_name]["slots"]:
             return False
         return True
 
-
-        # For ServiceRunBehaviour -- cordinate protocol
 
     class ServiceRunBehaviour(CyclicBehaviour):
 
@@ -72,8 +82,6 @@ class ServiceStationAgent(QueueStationAgent):
             super().__init__()
             #self.test = []
 
-        #Posible eliminación
-        #async def cancel_service(self, agent_id, content=None):
         async def refuse_service(self, agent_id, content=None):
             """
             Sends a ``spade.message.Message`` to a transport to accept a travel proposal for charge.
@@ -86,10 +94,7 @@ class ServiceStationAgent(QueueStationAgent):
             reply = Message()
             reply.to = str(agent_id)
             reply.set_metadata("protocol", REQUEST_PROTOCOL)
-            #reply.set_metadata("performative", ACCEPT_PERFORMATIVE)
-            #reply.set_metadata("performative", INFORM_PERFORMATIVE)
             reply.set_metadata("performative", REFUSE_PERFORMATIVE)
-            #content = {"station_id": self.agent.name}
             reply.body = json.dumps(content)
             await self.send(reply)
             logger.debug(
@@ -113,9 +118,7 @@ class ServiceStationAgent(QueueStationAgent):
             reply = Message()
             reply.to = str(agent_id)
             reply.set_metadata("protocol", REQUEST_PROTOCOL)
-            #reply.set_metadata("performative", ACCEPT_PERFORMATIVE)
             reply.set_metadata("performative", INFORM_PERFORMATIVE)
-            #content = {"station_id": self.agent.name}
             reply.body = json.dumps(content)
             await self.send(reply)
             logger.debug(
@@ -126,187 +129,56 @@ class ServiceStationAgent(QueueStationAgent):
             )
 
         async def on_start(self):
+            """
+                Called when the behavior starts, logging the event.
+            """
             logger.debug("Strategy {} started in station".format(type(self).__name__))
 
         async def run(self):
-
+            """
+                Main execution loop that processes the waiting lists and assigns services to agents
+                if slots are available. It dequeues agents from the queue and starts the respective service.
+            """
             template1 = Template()
             template1.set_metadata("protocol", REQUEST_PROTOCOL)
             template1.set_metadata("performative", INFORM_PERFORMATIVE)
 
-            # DEPURACION 2
-            # logger.warning(
-            #    "Service {} TIENE LA SIGUIENTE waitinglist {}".format(
-            #        self.agent.name,
-            #        self.agent.waiting_lists
-            #    )
-            # )
-
-            # if self.agent.service_available(self):
-            #    self.agent.queue_agent_to_waiting_list(service_name, str(agent_id))
-            #    self.agent.total_queue_size()
-
+            # Iterate through the available service types and their corresponding queues
             for service_name, queue in self.agent.waiting_lists.items():
 
                 if len(queue) > 0:
 
-                    # DEPURACION 6
-                    # logger.info(
-                    #    "SERVICESTATION - service_name {}".format(
-                    #        service_name
-                    #    )
-                    # )
-
                     if self.agent.service_available(service_name):
-                        # dequeue
-                        # agent_info = self.agent.dequeue_first_agent_to_waiting_list(service_name)        #Adaptar los ARGS -- APUNTES
+
+                        # Dequeue the first agent from the queue for the given service
                         agent_info = self.agent.queuebehaviour.dequeue_first_agent_to_waiting_list(service_name)
 
                         if agent_info is not None:
                             agent, kwargs = agent_info
 
-                            # DEPURACION 8
-                            logger.warning(
-                                "SERVICESTATION - Agent {}, slots usados: {}".format(
-                                    self.agent.name,
-                                    self.agent.get_slot_number_used(service_name)
-                                )
-                            )
-
+                            # Increase the number of slots in use for this service
                             self.agent.increase_slots_used(service_name)
 
+                            # Inform the agent that they are being served
                             content = {"station_id": self.agent.name, "serving": True}
                             await self.inform_service(str(agent), content)
 
-                            # DEPURACION 7
                             logger.info(
-                                "SERVICESTATION - agent: {}, args: {}, station slots: {}".format(
+                                "Agent: {} with args: {}, station slots used: {}".format(
                                     agent,
                                     kwargs,
                                     self.agent.get_slot_number_used(service_name)
                                 )
                             )
 
+                            # Get service-specific arguments
                             arguments_station = self.agent.show_service_arguments(service_name)
                             arguments_station["service_name"] = service_name
-                            logger.info(
-                                "SERVICESTATION 0.1 - arguments_station: {}".format(
-                                    arguments_station
-                                )
-                            )
                             kwargs.update(arguments_station)
 
-                            # Duda
+                            # Retrieve and instantiate the appropriate service behavior
                             one_shot_behaviour = self.agent.services_list[service_name]["one_shot_behaviour"]
-
-                            # DEPURACION 8
-                            logger.info(
-                                "SERVICESTATION 1 - one_shot_behaviour: {}".format(
-                                    one_shot_behaviour
-                                )
-                            )
-
                             one_shot_behaviour = one_shot_behaviour(str(agent), **kwargs)
 
-                            # DEPURACION 8
-                            logger.info(
-                                "SERVICESTATION 2 - one_shot_behaviour: {}".format(
-                                    one_shot_behaviour
-                                )
-                            )
-
-                            # DEPURACION 8
-                            # logger.info(
-                            #    "SERVICESTATION 3 - one_shot_behaviour: {}".format(
-                            #        one_shot_behaviour
-                            #    )
-                            # )
-
-                            self.agent.add_behaviour(one_shot_behaviour, template1)  # PASAR ARGS (self.agent.power)
-
-                            # TEST - NO FUNCIONA
-                            # logger.info(
-                            #    "Agent {} has finished receiving the service {}".format(
-                            #        self.agent.name,
-                            #        service_name
-                            #    )
-                            # )
-
-                            # Run service -- Duda
-
-                            # TEST - NO FUNCIONA
-                            # self.agent.decrease_slots_used(service_name)
-
-            # COPIA ORIGINAL
-            # for service_name, queue in self.agent.waiting_lists.items():
-            #
-            #     if len(queue) > 0:
-            #         if self.agent.service_available(self, service_name):
-            #             # dequeue
-            #             agent_info = self.agent.dequeue_first_agent_to_waiting_list(service_name)        #Adaptar los ARGS -- APUNTES
-            #
-            #             if agent_info is not None:
-            #                 agent, args = agent_info
-            #
-            #                 self.agent.increase_slots_used(service_name)
-            #
-            #
-            #                 #DEBATE - INFORMAR AGENTE DE QUE COMIENZA EL SERVICIO - SERVICIO_1a
-            #
-            #
-            #
-            #                 #Preguntar simulatorAgent para el near -
-            #                 #await self.inform_agent(str(agent))
-            #
-            #                 # Send msg to SimulatorAgent for agent_position
-            #                 self.agent.request_agent_position("simulator_none@localhost")
-            #
-            #                 # Request to SimulatorAgent for agent_position
-            #
-            #
-            #                 msg = await self.receive(timeout=5)
-            #                 content = json.loads(msg.body)
-            #
-            #                 if msg:
-            #                     performative = msg.get_metadata("performative")
-            #                     agent_id = msg.sender
-            #                     #agent_position = json.loads(msg.body)["agent_position"]
-            #                     agent_position = content["agent_position"]
-            #
-            #                     if not self.agent.near_agent(coords_1=self.agent.get_position(), coords_2=agent_position):
-            #                         logger.warning(
-            #                             "Station {} has Cancel request from agent {} for service {}".format(
-            #                                 self.agent.name,
-            #                                 agent_id,
-            #                                 service_name
-            #                             )
-            #                         )
-            #
-            #                         # Msg Cancel
-            #                         #self.cancel_service(str(agent))        #Original version
-            #
-            #                         #New version
-            #                         content = {"station_id": self.agent.name}
-            #                         self.refuse_service(str(agent), content)
-            #                     else:
-            #
-            #                         content = {"station_id": self.agent.name, "serving":True}
-            #                         self.inform_service(str(agent), content)
-            #
-            #                         # Duda
-            #                         one_shot_behaviour = self.agent.waiting_lists[service_name]["one_shot_behaviour"]
-            #                         self.agent.add_behaviour(one_shot_behaviour(agent_id=agent_id, *args), template1)        #PASAR ARGS (self.agent.power)
-            #
-            #
-            #
-            #                         logger.info(
-            #                             "Agent {} has finished receiving the service {}".format(
-            #                                 self.agent.name,
-            #                                 service_name
-            #                             )
-            #                         )
-            #
-            #                         # Run service -- Duda
-            #
-            #                         self.agent.decrease_slots_used(service_name)
-            #
+                            # Add the behavior to the agent
+                            self.agent.add_behaviour(one_shot_behaviour, template1)
